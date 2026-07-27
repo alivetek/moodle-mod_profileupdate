481185490c926e9c62d4f4fd350c42c4dfc9ac29
ertborTek <robert@alivetek.com>
2026-07-27 14:12:28 -0400

    Added a Moodle plugin CI workflow.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_profileupdate';
-$plugin->version   = 2026072701;
+$plugin->version   = 2026072702;
 $plugin->requires  = 2025100600;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.2.2';
+$plugin->release   = '0.2.3';
