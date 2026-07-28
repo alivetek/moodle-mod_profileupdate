1e7af84f50ec58aef65c45e1d1cc58327dd9890c
ertborTek <robert@alivetek.com>
2026-07-28 16:19:25 -0400

    Reordered language strings since Moodle coding standards wants them alphabetized.

--- lang/en/profileupdate.php
+++ lang/en/profileupdate.php
@@ -24,41 +24,37 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['configurefieldslink'] = 'Configure fields to update';
+$string['continuetocourse'] = 'Continue';
+$string['field_address'] = 'Address';
+$string['field_city'] = 'City/town';
+$string['field_country'] = 'Country';
+$string['field_department'] = 'Department';
+$string['field_description'] = 'Description';
+$string['field_email'] = 'Email address';
+$string['field_firstname'] = 'First name';
+$string['field_idnumber'] = 'ID number';
+$string['field_institution'] = 'Institution';
+$string['field_lastname'] = 'Last name';
+$string['field_phone1'] = 'Phone';
+$string['field_phone2'] = 'Mobile phone';
+$string['field_timezone'] = 'Timezone';
+$string['field_url'] = 'Web page';
+$string['fieldscategory_custom'] = 'Custom fields';
+$string['fieldscategory_standard'] = 'Standard fields';
+$string['fieldstoupdate'] = 'Profile fields to update';
+$string['fieldstoupdate_help'] = 'Select one or more profile fields that a student will be asked to update when opening this activity. Both standard user fields and custom profile fields are available for selection.';
+$string['fieldstoupdateintro'] = 'You will be asked to update the following profile fields:';
+$string['missingidandcmid'] = 'Missing instance id or course module id';
 $string['modulename'] = 'Profile Update';
+$string['modulenameplural'] = 'Profile Updates';
+$string['nofieldsselected'] = 'No profile fields have been configured for updating yet.';
+$string['pluginadministration'] = 'Profile Update administration';
+$string['pluginname'] = 'Profile Update';
+$string['privacy:metadata'] = 'The Profile Update plugin does not store any personal data. It only stores which profile fields an activity instance is configured to show, and updates the user\'s own profile through core Moodle APIs.';
 $string['profileupdate:addinstance'] = 'Add a new Profile Update activity';
 $string['profileupdate:manage'] = 'Configure the profile fields a Profile Update activity requires';
 $string['profileupdate:view'] = 'View Profile Update activity';
-$string['modulenameplural'] = 'Profile Updates';
-$string['pluginname'] = 'Profile Update';
-$string['pluginadministration'] = 'Profile Update administration';
-$string['profileupdatename'] = 'Name';
-$string['missingidandcmid'] = 'Missing instance id or course module id';
-$string['privacy:metadata'] = 'The Profile Update plugin does not store any personal data. It only stores which profile fields an activity instance is configured to show, and updates the user\'s own profile through core Moodle APIs.';
-
-// Profile fields selection.
-$string['fieldstoupdate'] = 'Profile fields to update';
-$string['fieldstoupdate_help'] = 'Select one or more profile fields that a student will be asked to update when opening this activity. Both standard user fields and custom profile fields are available for selection.';
-$string['fieldscategory_standard'] = 'Standard fields';
-$string['fieldscategory_custom'] = 'Custom fields';
-$string['nofieldsselected'] = 'No profile fields have been configured for updating yet.';
-$string['fieldstoupdateintro'] = 'You will be asked to update the following profile fields:';
-$string['configurefieldslink'] = 'Configure fields to update';
-$string['updateprofileintro'] = 'Please review and update the following profile fields:';
 $string['profileupdated'] = 'Your profile has been updated.';
-$string['continuetocourse'] = 'Continue';
-
-// Standard profile field labels.
-$string['field_firstname'] = 'First name';
-$string['field_lastname'] = 'Last name';
-$string['field_email'] = 'Email address';
-$string['field_city'] = 'City/town';
-$string['field_country'] = 'Country';
-$string['field_timezone'] = 'Timezone';
-$string['field_description'] = 'Description';
-$string['field_phone1'] = 'Phone';
-$string['field_phone2'] = 'Mobile phone';
-$string['field_institution'] = 'Institution';
-$string['field_department'] = 'Department';
-$string['field_address'] = 'Address';
-$string['field_idnumber'] = 'ID number';
-$string['field_url'] = 'Web page';
+$string['profileupdatename'] = 'Name';
+$string['updateprofileintro'] = 'Please review and update the following profile fields:';
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_profileupdate';
-$plugin->version   = 2026072803;
+$plugin->version   = 2026072805;
 $plugin->requires  = 2025100600;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.2.7';
+$plugin->release   = '0.2.9';
